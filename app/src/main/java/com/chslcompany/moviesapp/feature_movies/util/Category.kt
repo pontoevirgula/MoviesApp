b271a99dae9812b308598050c247dcc469eb515d
carlos.henrique <carlos.henrique@timwe.com>
2023-12-24 03:24:18 -0300

    criado classes util

---
+++ app/src/main/java/com/chslcompany/moviesapp/feature_movies/util/Category.kt
@@ -0,0 +1,6 @@
+package com.chslcompany.moviesapp.feature_movies.util
+
+object Category {
+    const val POPULAR = "popular"
+    const val UPCOMING = "upcoming"
+}
---
+++ app/src/main/java/com/chslcompany/moviesapp/feature_movies/util/Screen.kt
@@ -0,0 +1,9 @@
+package com.chslcompany.moviesapp.feature_movies.util
+
+sealed class Screen(val rout: String) {
+    data object Home : Screen("main")
+    data object PopularMovieList : Screen("popularMovie")
+    data object UpcomingMovieList : Screen("upcomingMovie")
+    data object Details : Screen("details")
+}
+
